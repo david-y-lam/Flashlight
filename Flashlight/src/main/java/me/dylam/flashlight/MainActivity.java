85a43d3997c79e7881063e89296d773b7138c578
David Lam <dylam07@gmail.com>
2013-12-22 23:45:45 -0800

    Added log message to button toggle

--- Flashlight/src/main/java/me/dylam/flashlight/MainActivity.java
+++ Flashlight/src/main/java/me/dylam/flashlight/MainActivity.java
@@ -110,6 +110,8 @@ public class MainActivity extends Activity {
     }
 
     public void toggleLightBtn(View v) {
+        Log.d(mTag, "Button clicked!");
+
         // React appropiately
         if (mToggleButton.isChecked()) {
             toggleOn();
